55537539e1b8b61a1b805e8e4572a6186ea4514e
Sreekrishna Sridhar <sreekrishna.sridhar@gmail.com>
2019-11-09 18:16:12 -0600

    Skeleton of server connecting with database

---
+++ server.js
@@ -0,0 +1,16 @@
+// attempt to connect to database
+
+let pool;
+const createPool = async() =>{
+	pool = await mysql.createPool({
+		user:
+		password: process.env.NBKpyMaG9e2wJuLC
+		database:process.env.utdsports
+		socketPath: 
+		
+
+});
+
+
+};
+createPool();

3907553b91ea292935b8d71fce757184bb7ed22f
mayukuse24 <vinaypoll@gmail.com>
2019-11-09 20:10:38 -0600

    updated mysql connector

--- server.js
+++ server.js
@@ -11,55 +11,24 @@ app.set('view engine','pug');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-
 //Responses
-
-let config = {
-	user:process.env.root,
-	database:process.env.utdsports,
-	password:process.env.NBKpyMaG9e2wJuLC
-
-
-}
-
-
-if(process.env.CONNECTION_NAME){
-	config.socketPath = ``;
-}
-
-let connection = mysql.createConnection(config);
-
-
-        connection.connect(function(err){
-
-        if(err){
-                console.error('Error connecting '+err.stack);
-                return;
-        }
-        console.log('Connected as thread id: '+connection.threadId);
-
-});
-
-/* Local checking
-let connection = mysql.createConnectin({
-
-	host:,
-	user:'root'
-	database:'utdsports',
-	password:''
-
-
-
-});
-	connection.connect(function(err){
-
-	if(err){
-		console.error('Error connecting '+err.stack);
-		return;
+module.exports = {
+	connect: function () {
+		let config = {
+			user: 'root',
+			database: 'utdsports',
+			password: '',
+			socketPath = `/cloudsql/utdsports:us-central1:instance-1`
+		};
+		
+		let connection = mysql.createConnection(config);
+		
+		connection.connect(function(err, conn){
+			if(err){
+					console.error('Error connecting '+err.stack);
+					return;
+			}
+			console.log('Connected as thread id: '+connection.threadId);
+		});
 	}
-	console.log('Connected as thread id: '+connection.threadId);
-
-});
-
-module.exports = connection;
-*/
+};

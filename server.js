a5aadbe2aef8a58b7822a28764bc1361b997b496
Sreekrishna Sridhar <sreekrishna.sridhar@gmail.com>
2019-11-09 19:10:59 -0600

    Attempt at a branch

--- server.js
+++ server.js
@@ -1,14 +1,54 @@
 // attempt to connect to database
 
-module.exports = {
-	let pool;
-	const createPool = async() =>{
-		pool = await mysql.createPool({
-			user: 'root',
-			password: '',
-			database: 'utdsports',
-			socketPath: '/cloudsql/utdsports:us-central1:instance-1'
-		});
-	};
-	createPool();
+'use strict';
+const express =  require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const app  = express();
+app.set('view engine','pug');
+
+//Parse request body
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+//Responses
+
+let config = {
+	user:process.env.root,
+	database:process.env.utdsports,
+	password:process.env.NBKpyMaG9e2wJuLC
+
+
 }
+
+
+if(process.env.CONNECTION_NAME){
+	config.socketPath = ``;
+}
+
+let connection = mysql.createConnection(config);
+
+/* Local checking
+let connection = mysql.createConnectin({
+
+	host:,
+	user:'root'
+	database:'utdsports',
+	password:''
+
+
+
+});
+	connection.connect(function(err){
+
+	if(err){
+		console.error('Error connecting '+err.stack);
+		return;
+	}
+	console.log('Connected as thread id: '+connection.threadId);
+
+});
+
+module.exports = connection;
+*/

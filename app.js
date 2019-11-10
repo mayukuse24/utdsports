3cbab155c92db26a9d8bccac2b110dc3803b9902
mayukuse24 <vinaypoll@gmail.com>
2019-11-09 18:56:30 -0600

    Basic structure of endpoints for test

--- app.js
+++ app.js
@@ -27,6 +27,55 @@ app.get('/', (req, res) => {
     .end();
 });
 
+app.post('/register', (req, res) => {
+  let netId,
+      password,
+      firstName,
+      lastName,
+      phoneNumber,
+      reqBody = _.get(req, 'body');
+    
+  // TODO: validate request
+  console.log(req.body);
+  
+  if (!reqBody) { return res.status(400).send('No body in request'); };
+
+  firstName = _.get(reqBody, 'firstName');
+  lastName = _.get(reqBody, 'lastName');
+  phoneNumber = _.get(reqBody, 'phoneNumber');
+  netId = _.get(reqBody, 'netId');
+  password = _.get(reqBody, 'password');
+
+  // TODO: hash password and store
+
+  // TODO: Write request to database
+  // conn.createUser()
+
+  res.status(200).send('OK');
+});
+
+app.post('/login', (req, res) => {
+  let netId,
+      password;
+
+  netId = _.get(reqBody, 'netId');
+  password = _.get(reqBody, 'password');
+
+  // TODO: retrieve password hash and confirm login
+
+  // TODO: test
+  res.status(200).send('OK');
+
+  if (hash(password) === db_password) {
+    // TODO: success response. Send user information
+    res.status(200).send('OK');
+  }
+});
+
+app.post('/creategame', (req, res) => {
+  
+});
+
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
